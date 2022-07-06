2f1ef866455b06f8c5db7c75b48454a2b8e1c025
Michael K <69758390+kootsZhin@users.noreply.github.com>
2022-07-06 15:36:11 -0400

    feat: added models

---
+++ src/utils.ts
@@ -0,0 +1,45 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+import { Config } from "./global";
+import { parseObligation, parseReserve, RESERVE_SIZE, OBLIGATION_SIZE } from "./models";
+import { findWhere } from 'underscore';
+
+export async function getReserves(connection: Connection, config: Config, lendingMarket: any) {
+    const resp = await connection.getProgramAccounts(new PublicKey(config.programID), {
+        commitment: connection.commitment,
+        filters: [
+            {
+                memcmp: {
+                    offset: 10,
+                    bytes: lendingMarket,
+                },
+            },
+            {
+                dataSize: RESERVE_SIZE,
+            },
+        ],
+        encoding: 'base64',
+    });
+
+    return resp.map((account) => parseReserve(account.pubkey, account.account));
+}
+
+export async function getObligation(connection: Connection, config: Config, lendingMarket) {
+    const resp = await connection.getProgramAccounts(new PublicKey(config.programID), {
+        commitment: connection.commitment,
+        filters: [
+            {
+                memcmp: {
+                    offset: 10,
+                    bytes: lendingMarket,
+                },
+            },
+            {
+                dataSize: OBLIGATION_SIZE,
+            },
+        ],
+        encoding: 'base64',
+    });
+
+    return resp.map((account) => parseObligation(account.pubkey, account.account));
+}
+
