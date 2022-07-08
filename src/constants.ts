f3e41399ef85e6df36b0ee7c6f2d589971a9d862
kootsZhin <69758390+kootsZhin@users.noreply.github.com>
2022-07-08 16:26:53 -0400

    fix: https

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const BASEURI = 'http://solana-lending-frontend.vercel.app/'
+export const BASEURI = 'https://solana-lending-frontend.vercel.app/'
 
 export const WAD = "1".concat(Array(18 + 1).join("0"));
